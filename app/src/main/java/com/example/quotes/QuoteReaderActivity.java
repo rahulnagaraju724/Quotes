7914e36d89e813670da77ad2a2d6ceae0ed6ba97
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-02-20 07:56:40 -0600

    Project setup 2

--- app/src/main/java/com/example/quotes/QuoteReaderActivity.java
+++ app/src/main/java/com/example/quotes/QuoteReaderActivity.java
@@ -1,10 +1,33 @@
 package com.example.quotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 
 public class QuoteReaderActivity extends AppCompatActivity {
+
+
+    public class QuoteAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int arg0) { return null; }
+
+        @Override
+        public long getItemId(int arg0) { return 0;  }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
