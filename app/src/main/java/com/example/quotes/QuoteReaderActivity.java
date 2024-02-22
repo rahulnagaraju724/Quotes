70dac29457885777eacd1352e930d070edebca20
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-02-22 07:53:38 -0600

    Demo

--- app/src/main/java/com/example/quotes/QuoteReaderActivity.java
+++ app/src/main/java/com/example/quotes/QuoteReaderActivity.java
@@ -3,10 +3,12 @@ package com.example.quotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 
 
@@ -71,6 +73,17 @@ public class QuoteReaderActivity extends AppCompatActivity {
 
         ListView mListView = findViewById(R.id.quotes_list);
         mListView.setAdapter(new QuoteAdapter(this));
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int
+                    position, long arg3) {
+                Intent i = new Intent(QuoteReaderActivity.this,
+                        QuoteDetail.class);
+                i.putExtra("position", position);
+                startActivity(i);
+            }
+        });
 
     }
 }
