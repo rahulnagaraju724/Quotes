b82ceea4a2f733c8c32f8551135999144bc37e66
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-02-24 22:18:48 -0600

    Splash screen added

--- app/src/main/java/com/example/quotes/Splash.java
+++ app/src/main/java/com/example/quotes/Splash.java
@@ -2,13 +2,22 @@ package com.example.quotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
+    private static final long SPLASH_SCREEN_TIMEOUT = 5000; // 5 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, QuoteReaderActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_TIMEOUT);
     }
 }
